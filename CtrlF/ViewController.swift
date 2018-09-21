c41a6473a91a05be5024932896463e16df53dd5f
Chethan (JP) <nh42858@hertz.net>
2018-09-21 15:25:18 -0400

    Initial Commit

---
+++ CtrlF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CtrlF
+//
+//  Created by Jeanette Pranin on 9/21/18.
+//  Copyright © 2018 Jeanette Pranin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CtrlFTests/CtrlFTests.swift
@@ -0,0 +1,34 @@
+//
+//  CtrlFTests.swift
+//  CtrlFTests
+//
+//  Created by Jeanette Pranin on 9/21/18.
+//  Copyright © 2018 Jeanette Pranin. All rights reserved.
+//
+
+import XCTest
+@testable import CtrlF
+
+class CtrlFTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
