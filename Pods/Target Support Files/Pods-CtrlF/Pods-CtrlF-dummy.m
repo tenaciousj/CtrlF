22f0d8167a1a10df80793a63963ddb065231cf3a
Chethan (JP) <nh42858@hertz.net>
2018-09-21 15:29:55 -0400

    Add Podfile, Add Tesseract pod, Create .gitignore file
    Add Podfile, Add Tesseract pod, Create .gitignore file

---
+++ Pods/Target Support Files/Pods-CtrlF/Pods-CtrlF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CtrlF : NSObject
+@end
+@implementation PodsDummy_Pods_CtrlF
+@end
---
+++ Pods/Target Support Files/Pods-CtrlFUITests/Pods-CtrlFUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CtrlFUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CtrlFUITests
+@end
